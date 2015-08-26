d61cfaabbb8583a5f118259bdd8ced440e58a68c
Michael Roitzsch <reactorcontrol@icloud.com>
2015-08-26 18:36:06 +0200

    code bookends
    implements the main entrypoint and some unresolved symbols

---
+++ src/crlinject.cpp
@@ -0,0 +1,29 @@
+#include "../contrib/security_ocspd/server/crlDb.cpp"
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2) {
+		printf("usage: crlinject URL < CRL\n");
+		exit(0);
+	}
+
+	// read CRL from stdin
+	std::istreambuf_iterator<char> eos;
+	std::string crl(std::istreambuf_iterator<char>(std::cin), eos);
+
+	CrlDatabase db;
+
+	CSSM_DATA crlData = { .Length = crl.length(), .Data = (uint8 *)crl.c_str() };
+	CSSM_DATA urlData = { .Length = ::strlen(argv[1]), .Data = (uint8 *)argv[1] };
+
+	CSSM_RETURN result = db.add(crlData, urlData);
+	if (result == CSSM_OK)
+		printf("successfully added CRL to cache\n");
+	else
+		printf("error code: %u\n", result);
+
+	return result;
+}
---
+++ src/stubs.cpp
@@ -0,0 +1,17 @@
+#include <security_cdsa_utilities/Schema.h>
+#include "../contrib/security_ocspd/server/ocspdNetwork.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+const CSSM_DB_SCHEMA_ATTRIBUTE_INFO Security::KeychainCore::Schema::X509CrlSchemaAttributeList[] = {};
+const CSSM_DB_SCHEMA_INDEX_INFO Security::KeychainCore::Schema::X509CrlSchemaIndexList[] = {};
+const uint32 Security::KeychainCore::Schema::X509CrlSchemaAttributeCount = 0;
+const uint32 Security::KeychainCore::Schema::X509CrlSchemaIndexCount = 0;
+
+extern "C"
+CSSM_RETURN ocspdNetFetch(Allocator &alloc, const CSSM_DATA &url, LF_Type lfType, CSSM_DATA &fetched)
+{
+	printf("ocspdNetFetch called\n");
+	abort();
+}
